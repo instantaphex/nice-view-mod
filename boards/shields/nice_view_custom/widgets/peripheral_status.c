1cca7451f44e3260d54890ad82f44b3cf0271af1
John Bryan <johnmorganbryan@gmail.com>
2025-11-30 18:03:13 -0500

    dither art

--- boards/shields/nice_view_custom/widgets/peripheral_status.c
+++ boards/shields/nice_view_custom/widgets/peripheral_status.c
@@ -25,7 +25,11 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 LV_IMG_DECLARE(balloon);
 LV_IMG_DECLARE(mountain);
-LV_IMG_DECLARE(akuma);
+
+LV_IMG_DECLARE(akuma_dither_1);
+LV_IMG_DECLARE(akuma_dither_2);
+LV_IMG_DECLARE(akuma_dither_3);
+LV_IMG_DECLARE(akuma_dither_4);
 
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 
@@ -118,7 +122,7 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
     lv_obj_t *art = lv_img_create(widget->obj);
     bool random = sys_rand32_get() & 1;
     // lv_img_set_src(art, random ? &balloon : &mountain);
-    lv_img_set_src(art, &akuma);
+    lv_img_set_src(art, &akuma_dither_1);
     lv_obj_align(art, LV_ALIGN_TOP_LEFT, 0, 0);
 
     sys_slist_append(&widgets, &widget->node);
