49c3ef79fe8030afa35b5e3b01f21f1bf0d72993
John Bryan <johnmorganbryan@gmail.com>
2025-11-30 18:22:26 -0500

    animation

--- boards/shields/nice_view_custom/widgets/peripheral_status.c
+++ boards/shields/nice_view_custom/widgets/peripheral_status.c
@@ -31,6 +31,13 @@ LV_IMG_DECLARE(akuma_dither_2);
 LV_IMG_DECLARE(akuma_dither_3);
 LV_IMG_DECLARE(akuma_dither_4);
 
+const lv_img_dsc_t *anim_imgs[] = {
+  &akuma_dither_1,
+  &akuma_dither_2,
+  &akuma_dither_3,
+  &akuma_dither_4
+};
+
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 
 struct peripheral_status_state {
@@ -119,10 +126,19 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
     lv_obj_align(top, LV_ALIGN_TOP_RIGHT, 0, 0);
     lv_canvas_set_buffer(top, widget->cbuf, CANVAS_SIZE, CANVAS_SIZE, LV_IMG_CF_TRUE_COLOR);
 
-    lv_obj_t *art = lv_img_create(widget->obj);
-    bool random = sys_rand32_get() & 1;
+    // static image showing
+    // lv_obj_t *art = lv_img_create(widget->obj);
+    // bool random = sys_rand32_get() & 1;
     // lv_img_set_src(art, random ? &balloon : &mountain);
-    lv_img_set_src(art, &akuma_dither_1);
+
+    // create animation
+    lv_obj_t * art = lv_animimg_create(widget->obj);
+    lv_obj_center(art);
+    lv_animimg_set_src(art, (const void **) anim_imgs, 12);
+    lv_animimg_set_duration(art, 4800);
+    lv_animimg_set_repeat_count(art, LV_ANIM_REPEAT_INFINITE);
+    lv_animimg_start(art);
+
     lv_obj_align(art, LV_ALIGN_TOP_LEFT, 0, 0);
 
     sys_slist_append(&widgets, &widget->node);
